8db254388c3b4001775be6abda8823b07bf36404
rahulchorotia <32265122+rahulchorotia@users.noreply.github.com>
2021-12-17 01:50:15 +0530

    Time: 299 ms (18.02%), Space: 76 MB (9.35%) - LeetHub

--- partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,9 +1,31 @@
 class Solution {
+    private:
+    
 public:
-    bool canPartition(vector<int>& nums) {
-        bitset<10001> bits(1);
-        int sum = accumulate(nums.begin(), nums.end(), 0);
-        for (auto n : nums) bits |= bits << n;
-        return !(sum & 1) && bits[sum >> 1];
+    int fun(int target,vector<int>&nums,int ind, vector<vector<int>>&dp)
+    {
+        if(target==0)
+            return 1;
+        if(ind==nums.size()||target<0)
+            return 0;
+        if(dp[ind][target]!=-1)
+            return dp[ind][target];
+        if(nums[ind]<=target)
+        return dp[ind][target]=max(fun(target-nums[ind],nums,ind+1,dp),fun(target,nums,ind+1,dp));
+       else 
+           return dp[ind][target]=fun(target,nums,ind+1,dp);
+    }
+    bool canPartition(vector<int>& nums) 
+    {
+        int n=nums.size();
+        int sum=0;
+        for(int x:nums)
+            sum+=x;
+        if(sum%2==1)
+            return false;
+        int target=sum/2;
+        vector<vector<int>>dp(n+1,vector<int>(target+1,-1));
+        int y=fun(target,nums,0,dp);
+        return y;
     }
 };
