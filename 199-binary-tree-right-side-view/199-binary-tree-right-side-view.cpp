712a4b92817a2ae777b9886b2fd38dd818c8b913
rahulchorotia <32265122+rahulchorotia@users.noreply.github.com>
2022-01-26 03:00:02 +0530

    Time: 4 ms (65.28%), Space: 11.9 MB (89.03%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -27,7 +27,7 @@ public:
             for(int i=0;i<n;i++){
                 TreeNode *temp=q.front();
                 q.pop();
-                if(i==n-1)temp1=temp->val;
+                temp1=temp->val;
                 if(temp->left)
                     q.push(temp->left);
                 if(temp->right)
