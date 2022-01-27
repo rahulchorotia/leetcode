bacd142b7b9f9d1a5a66fa058584b635be13b3c1
rahulchorotia <32265122+rahulchorotia@users.noreply.github.com>
2022-01-27 21:13:42 +0530

    Time: 12 ms (72.09%), Space: 21.8 MB (31.32%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -1,38 +1,34 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-typedef long long int lli;
+
 class Solution {
 public:
-   
-    bool isValidBST(TreeNode* root,lli ll=LLONG_MIN,lli rl=LLONG_MAX) {
-        if(!root->left&&!root->right)   return 1;
-        
-        if(root->left and root->right)
+    bool isValidBST(TreeNode* root,TreeNode * mini=NULL,TreeNode *maxi=NULL) 
+    {
+        if(!root)
+            return 1;
+        if(mini && maxi)
         {
-            bool l=(root->val>root->left->val)&&(root->left->val>ll)?1:0;
-            bool r=(root->val<root->right->val)&&(root->right->val<rl)?1:0;
-            if(l&&r)
-                return isValidBST(root->left,ll,root->val)&&isValidBST(root->right,root->val,rl);
-            return false;
+            if(root->val<=mini->val||root->val>=maxi->val)
+                return 0;
+             
+              return isValidBST(root->left,mini,root)&&isValidBST(root->right,root,maxi);
         }
-        if(root->left)
+        if(mini&&!maxi)
         {
-            if((root->val > root->left->val)&&(root->left->val>ll))
-             return isValidBST(root->left,ll,root->val);
-            return 0;
+            if(root->val<=mini->val)
+                return 0;
+            else
+                return isValidBST(root->left,mini,root)&&isValidBST(root->right,root,maxi);
         }
-          
-            if((root->val <root->right->val)&&(root->right->val<rl))
-             return isValidBST(root->right,root->val,rl);
-            return 0;     
+        if(!mini&&maxi){
+            
+            if(root->val>=maxi->val)
+                return 0;
+            else
+                return isValidBST(root->left,mini,root)&&isValidBST(root->right,root,maxi);
+        }
+        if(!mini && !maxi)
+            return isValidBST(root->left,mini,root)&&isValidBST(root->right,root,maxi);
+       return 0;
     }
+    
 };
