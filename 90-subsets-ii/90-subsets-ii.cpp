cb3a1a74becd8fe2a41db708ef49f4fc29822609
rahulchorotia <32265122+rahulchorotia@users.noreply.github.com>
2022-02-20 23:18:46 +0530

    Time: 16 ms (12.60%), Space: 17.6 MB (8.13%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -5,7 +5,7 @@ public:
     {
         if(i==n)
         {
-            sort(d.begin(),d.end());
+            //sort(d.begin(),d.end());
             if(s[d]==0)
             {
             ans.push_back(d);
@@ -19,6 +19,7 @@ public:
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
       
+        sort(nums.begin(),nums.end());
         int i=0;
         int j=nums.size();
         vector<vector<int>> ans;
