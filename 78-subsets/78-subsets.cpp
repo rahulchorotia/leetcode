534db42c1573dcef6a887a35cab250bfbe353841
rahulchorotia <32265122+rahulchorotia@users.noreply.github.com>
2022-02-20 22:31:21 +0530

    Time: 8 ms (18.13%), Space: 7.4 MB (39.52%) - LeetHub

---
+++ 78-subsets/78-subsets.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    set<vector<int>>st;
+    vector<vector<int>> subsets(vector<int>& nums) {
+       vector<int> ans;
+        vector<vector<int>>res;
+        fun(ans,nums,0);
+        for(auto &x:st){
+            res.push_back(x);
+        }
+        return res;
+    }
+    void fun(vector<int> &ans,vector<int>&nums,int i){
+       if(i>nums.size())
+           return;
+        st.insert(ans);
+        if(i>=nums.size())
+           return;
+        ans.push_back(nums[i]);
+        fun(ans,nums,i+1);
+        ans.pop_back();
+        fun(ans,nums,i+1);
+        
+    }
+};
