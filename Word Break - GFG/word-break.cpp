251e51a328dd255268e4442832ab623d2780d0c0
rahulchorotia <32265122+rahulchorotia@users.noreply.github.com>
2022-02-21 17:38:41 +0530

    Added solution - LeetHub

---
+++ Word Break - GFG/word-break.cpp
@@ -0,0 +1,61 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+
+// A : given string to search
+// B : vector of available strings
+
+class Solution
+{
+public:
+unordered_map<string,int>mp;
+    bool solve(string A){
+        if(A.size()==0)
+            return 1;
+      bool a=0;string temp="";
+      for(int i=0;i<A.length();i++){
+          
+          
+          temp+=A[i];
+          if(mp.find(temp)!=mp.end())
+          {
+              a=a||solve(A.substr(i+1));
+          }
+      }
+      return a;
+    }
+    int wordBreak(string A, vector<string> &B) {
+       for(int i=0;i<B.size();i++){
+           mp[B[i]]++;
+       }
+        return solve(A);
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<string> dict;
+        for(int i=0;i<n;i++){
+            string S;
+            cin>>S;
+            dict.push_back(S);
+        }
+        string line;
+        cin>>line;
+        Solution ob;
+        cout<<ob.wordBreak(line, dict)<<"\n";
+    }
+}
+  // } Driver Code Ends
