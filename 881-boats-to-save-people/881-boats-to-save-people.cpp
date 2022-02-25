320eecce981cd09b7b8eac73f9d18a1d4bedc12b
rahulchorotia <32265122+rahulchorotia@users.noreply.github.com>
2022-02-25 15:25:42 +0530

    Time: 68 ms (95.49%), Space: 42 MB (9.01%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.cpp
+++ 881-boats-to-save-people/881-boats-to-save-people.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
-    int solve(vector<int>&p,int lm,int cnt,int i,int j){
+    int solve(vector<int>&p,int lm,int &cnt,int i,int j){
         if(i>j)
             return cnt;
         if(i==j){
             return cnt+1;
         }
+        cnt++;
         if(p[i]+p[j]>lm){
-            return solve(p,lm,cnt+1,i,j-1);
+            
+            return solve(p,lm,cnt,i,j-1);
         }
         else if(p[i]+p[j]<=lm){
-            return solve(p,lm,cnt+1,i+1,j-1);
+            return solve(p,lm,cnt,i+1,j-1);
         }
         return 0;
     }
