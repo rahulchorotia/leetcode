0ab9520f6472ce77ace504c829334981ec2f8201
rahulchorotia <32265122+rahulchorotia@users.noreply.github.com>
2022-03-15 13:48:08 +0530

    Time: 16 ms (85.82%), Space: 16.6 MB (51.72%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,30 +1,35 @@
 class Solution {
 public:
-     int solve(vector<int>& arr,int k,int n){
-      if(k==0)
-        return 0;
-      int index=-1;
-      for(int i=0;i<k;i++){
-          if(arr[i]>=(k-i))
-          {
-              index=i;
-              break;
-          }
-      }
-      
-      if(index == -1)
-        return -1;
-        int dis = solve(arr,index,n);
-        if(dis ==-1)
-        return -1;
-        return 1+dis;
-  }
-    int jump(vector<int>& nums) 
+  
+    int jump(vector<int>& arr) 
     {
-        int n=nums.size();
-       
+        int n = arr.size();
+        if(n == 1)
+            return 0;
+        int curr_max = 0;
+        //int max_ind = 0;
+        int i = 0;
+        int step = 0;
         
-        return solve(nums,n-1,n);
+        while(i < n){
+            
+            step++;
+            if(arr[i] + i >= n-1)
+                return step;
+            if(arr[i] == 0)
+                return -1;
+            curr_max = i+1;
+            int w = 0;
+            for(int j = i+1 ; j <= arr[i]+i;j++){
+                if(arr[j] != 0 && arr[curr_max] <= arr[j]+w){
+                    curr_max = j;
+                    w = 0;
+                }
+                w++;
+            }
+            i = curr_max;
+        }
+        return step;
         
     }
         
